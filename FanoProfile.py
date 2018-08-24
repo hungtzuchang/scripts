58735bd6a5038f02e3261854f66edad8e5e95753
Hung-Tzu Chang <hungtzu.chang@gmail.com>
2018-08-23 19:12:21 -0700

    add Lorentz to Fano and Gaussian convolution

--- FanoProfile.py
+++ FanoProfile.py
@@ -15,3 +15,13 @@ def FanoProfile(Evector,E0,Gamma,q):
 def Lorentzian(Evector,E0,Gamma):
 	return Gamma/2/(np.square(Evector-E0)+np.square(Gamma/2))
 
+def FanoUnifiedPhase(Evector,E0,Gamma,phi):
+	phi=(phi + np.pi) % (2 * np.pi ) - np.pi
+	Evectornew=(Evector-E0)*2/Gamma
+	if(np.abs(phi)<1e-6):
+		return Gamma/2/(np.square(Evector-E0)+np.square(Gamma/2))
+	else:
+		q=1./np.tan(phi)
+		return (np.square(Evectornew+q)/(1+np.square(Evectornew)))
+		
+
--- Gauss_conv.py
+++ Gauss_conv.py
@@ -3,7 +3,17 @@ from scipy.signal import fftconvolve
 from scipy.stats import norm
 
 def Gauss_conv(v,broadening,spacing):
-    gauss_axis=np.arange(-3*v.shape[0]/2,3*v.shape[0]/2)*(spacing)
-    gauss_Fe3=norm.pdf(gauss_axis,0,broadening)
-    Fe3spec=fftconvolve(v,gauss_Fe3,'same')*(spacing)
-    return Fe3spec
+	gauss_axis=np.arange(-3*v.shape[0]/2,3*v.shape[0]/2)*(spacing)
+	vnew=np.r_[v[::-1],v,v[::-1]]
+	gauss_Fe3=norm.pdf(gauss_axis,0,broadening)
+	Fe3spec=fftconvolve(vnew,gauss_Fe3,'same')*(spacing)
+	Fe3spec=Fe3spec[v.shape[0]:2*v.shape[0]]
+	return Fe3spec
+    
+if __name__ == "__main__":
+	import matplotlib.pyplot as plt
+	x=np.linspace(-10,10,201)
+	y=1/(1+np.square(x))
+	plt.plot(x,y)
+	plt.plot(x,Gauss_conv(y,1,x[1]-x[0]))
+	plt.show()
